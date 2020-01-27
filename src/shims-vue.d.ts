cbcca4c7dc7c6001e2f89d126b85f43b934c178f
Derek Pedersen <derekpedersen.com@gmail.com>
2020-01-26 16:01:47 -0800

    Fixed MdIcon issue along with some others

--- src/shims-vue.d.ts
+++ src/shims-vue.d.ts
@@ -8,7 +8,12 @@ declare module "vue-material/dist/components" {
   import _Vue from "vue"; // <-- notice the changed import
 
   // export type PluginFunction<T> = (Vue: typeof _Vue, options?: T) => void;
+  export function MdApp(Vue: typeof _Vue, options?: any): void 
   export function MdButton(Vue: typeof _Vue, options?: any): void 
-  export function MdContent(Vue: typeof _Vue, options?: any): void 
+  export function MdContent(Vue: typeof _Vue, options?: any): void
+  export function MdDrawer(Vue: typeof _Vue, options?: any): void 
+  export function MdIcon(Vue: typeof _Vue, options?: any): void   
+  export function MdList(Vue: typeof _Vue, options?: any): void  
   export function MdTabs(Vue: typeof _Vue, options?: any): void 
+  export function MdToolbar(Vue: typeof _Vue, options?: any): void 
 }
