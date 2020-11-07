7d166c7cfb0484e67b9769179ef460f188dec1e0
Derek Pedersen <derekpedersen.com@gmail.com>
2020-11-06 16:09:50 -0800

    Added a icon ribbon to the nav

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,16 @@
 <template>
   <div id="app">
     <md-tabs md-sync-route>
-      <md-tab id="tab-home" md-label="Home" to="/" exact></md-tab>
-      <md-tab id="tab-about" md-label="About" to="/about" exact></md-tab>
-      <md-tab id="tab-family" md-label="Family" to="/family" exact></md-tab>
+      <md-tab id="tab-home" md-label="Home" to="/" exact md-icon="home"></md-tab>
+      <md-tab id="tab-about" md-label="About" md-icon="info" to="/about" exact></md-tab>
+      <md-tab id="tab-family" md-label="Family" md-icon="people" to="/family" exact></md-tab>
+      <md-tab id="github" md-label="Github" md-alignment="right" md-icon="/assets/icons/github-box.svg"></md-tab>
+      <md-tab id="linkedin" md-label="Linkedin" md-icon="/assets/icons/linkedin-box.svg"></md-tab>
+      <md-tab id="docker" md-label="Docker" md-icon="/assets/icons/docker.svg"></md-tab>
+      <md-tab id="stackoverflow" md-label="Stackoverflow" md-icon="/assets/icons/stackoverflow.svg"></md-tab>
+      <md-tab id="hackerrank" md-label="Hackerrank" md-icon="/assets/icons/hackerrank.svg"></md-tab>
+      <md-tab id="codefights" md-label="Codefights" md-icon="/assets/icons/codefights.svg"></md-tab>
+      <md-tab id="udemy" md-label="Udemy" md-icon="/assets/icons/udemy.svg"></md-tab>
     </md-tabs>
     <router-view />
   </div>
@@ -35,9 +42,10 @@
 <script lang="ts">
 import Vue from "vue";
 import Component from "vue-class-component";
-import { MdTabs } from "vue-material/dist/components";
+import { MdIcon, MdTabs } from "vue-material/dist/components";
 
 Vue.use(MdTabs);
+Vue.use(MdIcon)
 
 const AppProps = Vue.extend({
   props: {
@@ -47,7 +55,6 @@ const AppProps = Vue.extend({
 
 @Component({
   components: {
-    
   }
 })
 export default class App extends AppProps {
