28ea3d652b025a744566514a10017391c1fe1a78
Derek Pedersen <derekpedersen.com@gmail.com>
2020-11-06 16:28:43 -0800

    Added links to external sites

--- src/App.vue
+++ src/App.vue
@@ -4,13 +4,13 @@
       <md-tab id="tab-home" md-label="Home" to="/" exact md-icon="home"></md-tab>
       <md-tab id="tab-about" md-label="About" md-icon="info" to="/about" exact></md-tab>
       <md-tab id="tab-family" md-label="Family" md-icon="people" to="/family" exact></md-tab>
-      <md-tab id="github" md-label="Github" md-alignment="right" md-icon="/assets/icons/github-box.svg"></md-tab>
-      <md-tab id="linkedin" md-label="Linkedin" md-icon="/assets/icons/linkedin-box.svg"></md-tab>
-      <md-tab id="docker" md-label="Docker" md-icon="/assets/icons/docker.svg"></md-tab>
-      <md-tab id="stackoverflow" md-label="Stackoverflow" md-icon="/assets/icons/stackoverflow.svg"></md-tab>
-      <md-tab id="hackerrank" md-label="Hackerrank" md-icon="/assets/icons/hackerrank.svg"></md-tab>
-      <md-tab id="codefights" md-label="Codefights" md-icon="/assets/icons/codefights.svg"></md-tab>
-      <md-tab id="udemy" md-label="Udemy" md-icon="/assets/icons/udemy.svg"></md-tab>
+      <md-tab id="github" md-label="Github" md-alignment="right" md-icon="/assets/icons/github-box.svg" href="https://github.com/derekpedersen" target="_blank" exact></md-tab>
+      <md-tab id="linkedin" md-label="Linkedin" md-icon="/assets/icons/linkedin-box.svg" href="https://www.linkedin.com/in/derek-pedersen-67105415/"></md-tab>
+      <md-tab id="docker" md-label="Docker" md-icon="/assets/icons/docker.svg" href="https://hub.docker.com/u/derekpedersen" target="_blank"></md-tab>
+      <md-tab id="stackoverflow" md-label="Stackoverflow" md-icon="/assets/icons/stackoverflow.svg" href="https://stackoverflow.com/users/1304353/derek-pedersen"></md-tab>
+      <md-tab id="hackerrank" md-label="Hackerrank" md-icon="/assets/icons/hackerrank.svg" href="https://www.hackerrank.com/derekpedersen"></md-tab>
+      <md-tab id="codefights" md-label="Codefights" md-icon="/assets/icons/codefights.svg" href="https://app.codesignal.com/profile/drockem"></md-tab>
+      <md-tab id="udemy" md-label="Udemy" md-icon="/assets/icons/udemy.svg" href="https://www.udemy.com/user/derek-pedersen-2/"></md-tab>
     </md-tabs>
     <router-view />
   </div>
