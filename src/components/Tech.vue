a5f72630f96289710528c30e94e8ec1e0b4f0c0e
Derek Pedersen <derekpedersen.com@gmail.com>
2021-12-13 15:07:51 -0800

    Updated Tech, but really need to update it to use an API

--- src/components/Tech.vue
+++ src/components/Tech.vue
@@ -1,5 +1,6 @@
 <template>
   <md-card>
+    <!-- TODO: table should be populated by a call to Resume API -->
     <md-table>
       <md-table-row>
         <md-table-head>Tech</md-table-head>
@@ -8,34 +9,51 @@
       </md-table-row>
       <md-table-row>
         <md-table-cell>Golang</md-table-cell>
-        <md-table-cell md-numeric>{{ yearsExperience(2017) }}</md-table-cell>
+        <md-table-cell md-numeric
+          >2016 - present ({{ yearsExperience(2016) }})</md-table-cell
+        >
         <md-table-cell
           >Simply the best. What you see is what you get.</md-table-cell
         >
       </md-table-row>
       <md-table-row>
         <md-table-cell>Kubernetes</md-table-cell>
-        <md-table-cell md-numeric>{{ yearsExperience(2017) }}</md-table-cell>
+        <md-table-cell md-numeric
+          >2017 - present ({{ yearsExperience(2017) }})</md-table-cell
+        >
+        <md-table-cell>As easy as Sunday mornings.</md-table-cell>
+      </md-table-row>
+      <md-table-row>
+        <md-table-cell>Helm</md-table-cell>
+        <md-table-cell md-numeric
+          >2019 - present ({{ yearsExperience(2019) }})</md-table-cell
+        >
         <md-table-cell>As easy as Sunday mornings.</md-table-cell>
       </md-table-row>
       <md-table-row>
         <md-table-cell>Docker</md-table-cell>
-        <md-table-cell md-numeric>{{ yearsExperience(2016) }}</md-table-cell>
-        <md-table-cell>Is there any other container?</md-table-cell>
+        <md-table-cell md-numeric
+          >2015 - present ({{ yearsExperience(2015) }})</md-table-cell
+        >
+        <md-table-cell></md-table-cell>
       </md-table-row>
       <md-table-row>
         <md-table-cell>Java</md-table-cell>
-        <md-table-cell md-numeric>{{ yearsExperience(2019) }}</md-table-cell>
+        <md-table-cell md-numeric
+          >2019 - present ({{ yearsExperience(2019) }})</md-table-cell
+        >
         <md-table-cell>1 billion devices and contracting.</md-table-cell>
       </md-table-row>
       <md-table-row>
         <md-table-cell>Ruby on Rails</md-table-cell>
-        <md-table-cell md-numeric>{{ yearsExperience(2019) }}</md-table-cell>
+        <md-table-cell md-numeric
+          >2019 - present ({{ yearsExperience(2019) }})</md-table-cell
+        >
         <md-table-cell>I don't like Ruby on Rails.</md-table-cell>
       </md-table-row>
       <md-table-row>
         <md-table-cell>C#</md-table-cell>
-        <md-table-cell md-numeric>6</md-table-cell>
+        <md-table-cell md-numeric>2012 - 2018 (6)</md-table-cell>
         <md-table-cell
           >Spent a lot of time with .NET starting at 3.0 till
           4.6.</md-table-cell
@@ -43,10 +61,40 @@
       </md-table-row>
       <md-table-row>
         <md-table-cell>Visual Basic</md-table-cell>
-        <md-table-cell md-numeric>2</md-table-cell>
+        <md-table-cell md-numeric>2012 - 2018 (6)</md-table-cell>
         <md-table-cell
           >If you work in a .NET shop that existed before 2010, you will
           encounter Visual Basic.</md-table-cell
+        >
+      </md-table-row>
+      <md-table-row>
+        <md-table-cell>Angular</md-table-cell>
+        <md-table-cell md-numeric
+          >2014 - present ({{ yearsExperience(2014) }})</md-table-cell
+        >
+        <md-table-cell
+          >Dealt with the 1.0, 1.5+, and 2.0+ changes and it's still probably my
+          favorite frontend framework.</md-table-cell
+        >
+      </md-table-row>
+      <md-table-row>
+        <md-table-cell>React</md-table-cell>
+        <md-table-cell md-numeric
+          >2017 - present ({{ yearsExperience(2017) }})</md-table-cell
+        >
+        <md-table-cell
+          >It has grown on me, but I'd probably pick Vue over React for a
+          greenfield project.</md-table-cell
+        >
+      </md-table-row>
+      <md-table-row>
+        <md-table-cell>Vue</md-table-cell>
+        <md-table-cell md-numeric
+          >2019 - present ({{ yearsExperience(2019) }})</md-table-cell
+        >
+        <md-table-cell
+          >If only putting together a small frontend this is now my default
+          choice.</md-table-cell
         >
       </md-table-row>
     </md-table>
@@ -62,7 +110,7 @@ import {
   MdCard,
   MdTable,
   MdRipple,
-  MdContent
+  MdContent,
 } from "vue-material/dist/components";
 
 Vue.use(MdCard);
@@ -72,18 +120,18 @@ Vue.use(MdContent);
 
 const TechProps = Vue.extend({
   props: {
-    propMessage: String
+    propMessage: String,
   },
   methods: {
     yearsExperience: function (startYear: number) {
       return new Date().getFullYear() - startYear;
     },
-  }
+  },
 });
 
 @Component({
   name: "Tech",
-  components: {}
+  components: {},
 })
 export default class Tech extends TechProps {}
 </script>
